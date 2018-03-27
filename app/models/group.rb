dbb45e1ff0261d0a95d90f8fd642090ef082af59
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-03-27 19:51:37 +0900

    Edit association and nested association
    問題1：グループのモデルとコントローラを作成をしましょう
    
    model group
    To allow destroying record of members table when group is destroyed.
    
    Model users
    Same as above
    
    Model members
    normal association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ApplicationRecord
+  has_many :members, dependent: :destroy
+  has_many :users,      through:   :members
+
+  accepts_nested_attributes_for :members, allow_destroy: true
 end
