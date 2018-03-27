23c43667cd91b4ba22b690be960f0082181d7985
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-03-27 20:40:38 +0900

    routing groups
     問題2：ルーティングを設定しましょう

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :groups
   devise_for :users
 root to: 'messages#index'
   get 'messages/index'
-  resources :users, only: [:edit, :update]
+  resources :users,  only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
