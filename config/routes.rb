80bb4098be749bf3994483e55630f95c0897f8de
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-03-27 15:00:15 +0900

    Edit routing only

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 root to: 'messages#index'
   get 'messages/index'
-  resources :users, :only => [:edit, :update]
-
+  resources :users, only: [:edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
