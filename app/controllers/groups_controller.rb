551610d60a50fb7a4990f4930174607c8de7df12
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-03-27 20:47:21 +0900

    Edit controller of groups
    delete unnecessary actions
    3. 該当するアクションをコントローラに定義する

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,17 +1,6 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:show, :edit, :update, :destroy]
 
-  # GET /groups
-  # GET /groups.json
-  def index
-    @groups = Group.all
-  end
-
-  # GET /groups/1
-  # GET /groups/1.json
-  def show
-  end
-
   # GET /groups/new
   def new
     @group = Group.new
@@ -48,16 +37,6 @@ class GroupsController < ApplicationController
         format.html { render :edit }
         format.json { render json: @group.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /groups/1
-  # DELETE /groups/1.json
-  def destroy
-    @group.destroy
-    respond_to do |format|
-      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
