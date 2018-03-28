f88f505577c344a227b394d99008c0fb674f3e34
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-03-28 18:13:16 +0900

    set validates to group model
    name column must be presence.

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,6 @@ class Group < ApplicationRecord
   has_many :users,      through:   :members
 
   accepts_nested_attributes_for :members, allow_destroy: true
+
+  validates :name, presence: true
 end
