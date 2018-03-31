d254bf5a1f35652586cbd388b32d2d0310a445e3
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-03-31 12:15:18 +0900

    delete group instance, and use argument for file sharing

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,6 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:show, :edit, :update, :destroy]
 
   def index
-    @groups = current_user.groups
   end
 
   # GET /groups/new
