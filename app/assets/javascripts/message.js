262321805e6247a203295ce9c6653bf667725933
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-04-06 20:29:30 +0900

    Edit message.js to add the HTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,24 @@
 $(function(){
+  function buildHTML(message){
+    var html =  `<div id="scroll-bottom" class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <div class="lower-message__text">
+                      ${message.content}<br>
+                      <img src="${message.image}">
+                    </div>
+                  </div>
+                </div>`
+    return html;
+  }
+
   $("#new_message").on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
