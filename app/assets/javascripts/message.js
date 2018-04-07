f0f5b2ff53bcbf8598f1a5f3f342bed63fe73a46
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-04-07 17:15:32 +0900

    Add semi colon to finish the process.
    After add semi colon, the return false became not necessary.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -22,7 +22,7 @@ $(function(){
   $("#new_message").on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -33,15 +33,13 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $(".messages").append(html)
-      $(".form__text").val('')
-      $(".image-button__image").val('')
       $('.messages').animate({scrollTop: $(".messages").scrollTop()+$("#scroll-bottom").position().top}), 200, 'swing';
-
+      $(".messages").append(html);
+      $(".form__text").val('');
+      $(".image-button__image").val('');
     })
     .fail(function(){
       alert('error');
     })
-    return false;
   });
 });
