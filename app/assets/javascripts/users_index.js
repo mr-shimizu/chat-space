2f4eacd19cf5f31591437fbec36e47f74e275f07
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-04-12 12:38:01 +0900

    Edit js file to append HTML of user information below text field.

--- app/assets/javascripts/users_index.js
+++ app/assets/javascripts/users_index.js
@@ -1,5 +1,14 @@
 $(function(){
   var user_list = $('#user-search-result')
+
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+              </div>`
+    user_list.append(html);
+  }
+
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
     $.ajax({
@@ -12,6 +21,7 @@ $(function(){
       user_list.empty();
       if (users.length !== 0) {
        users.forEach(function(user){
+         appendUser(user);
        });
      }
       // usersは配列オブジェクト,中にはハッシュオブジェクト[{id: , name:}, {id: , name:}, {id: , name:}]。ただし、usersからuserを取り出した後はuser.id, user.nameで出来る。
