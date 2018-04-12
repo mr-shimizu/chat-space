f7d32a2613191a33be67d672b5f04d905254d88c
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-04-12 12:36:01 +0900

    Edit js file to add ".done"

--- app/assets/javascripts/users_index.js
+++ app/assets/javascripts/users_index.js
@@ -7,5 +7,12 @@ $(function(){
       data: { keyword: input },
       dataType: 'json'
     })
+    .done(function(users){
+      if (users.length !== 0) {
+       users.forEach(function(user){
+       });
+     }
+      // usersは配列オブジェクト,中にはハッシュオブジェクト[{id: , name:}, {id: , name:}, {id: , name:}]。ただし、usersからuserを取り出した後はuser.id, user.nameで出来る。
+    })
   });
 });
