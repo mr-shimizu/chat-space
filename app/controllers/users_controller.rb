012acd5c40c0d7596a20b37ff219b23994e88c8a
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-04-12 14:14:24 +0900

    Edit controller to add "unless" process which avoid the error(*1)
    (*1) When write in text field and delete what have written, error
    occurs that all the user info will appear under text field.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
 
 def index
-   @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
-   respond_to do |format|
-     format.html
-     format.json
+  @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20) unless params[:keyword] == ""
+  respond_to do |format|
+    format.html
+    format.json
    end
 end
 
