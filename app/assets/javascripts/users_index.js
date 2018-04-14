7f676e9ad6ff35f1f57a89221570be4c20c5ba2e
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-04-14 12:04:16 +0900

    Edit js file to define "appendGroupUser" action
    By having user's info which contains user's id, and name, the html is
    created.
    Then append the html to view file.

--- app/assets/javascripts/users_index.js
+++ app/assets/javascripts/users_index.js
@@ -9,6 +9,14 @@ $(function(){
               </div>`
     user_list.append(html);
   }
+  function appendGroupUser(user){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                <input name='group[user_ids][]' type='hidden' value='${user.id}'>
+                <p class='chat-group-user__name'>${user.name}</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>`
+    group_user_list.append(html);
+  };
 
   user_field.on("keyup", function(){
     var input = user_field.val();
